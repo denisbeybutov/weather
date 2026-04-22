fb90b75fe772d20b33a4a700a8841cca90989da3
denisbeybutov <beybutov.denis@mail.ru>
2026-04-22 19:21:02 +0300

    процесс верстки

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 import { getWeatherData } from "./api.js"
+import { createHeader } from "./appHeader.js";
 
 const app = async () => {
-    const weather = await getWeatherData(JSON.parse(localStorage.getItem('city')) || 'Москва');
+    const weather = await getWeatherData('Москва');
+    const header = createHeader(weather.name);
+    document.body.append(header);
     console.log(weather);
 }
 
