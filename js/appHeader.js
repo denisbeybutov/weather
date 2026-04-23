03cab19fad1a375f7b44b576ee94016f48905aa1
denisbeybutov <beybutov.denis@mail.ru>
2026-04-23 13:00:05 +0300

    1

--- js/appHeader.js
+++ js/appHeader.js
@@ -66,7 +66,7 @@ export const createHeader = (city) => {
 
             resetWeatherContent(weather.name, weather);
         } catch (error){
-            console.log(error);
+            
         }
     })
 
